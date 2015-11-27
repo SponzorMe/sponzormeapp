b89f13429beb3c12b12a6b634d1b81a772805459
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2015-11-26 20:07:25 -0500

    Sponsors list and Filter event, close #13

--- www/app/sponsors-organizer/sponzor-list.controller.js
+++ www/app/sponsors-organizer/sponzor-list.controller.js
@@ -14,21 +14,37 @@
   SponzorListController.$inject = [
     '$localStorage',
     'sponzorshipService',
-    'utilsService'
+    'utilsService',
+    '$ionicPopover',
+    '$scope'
   ];
 
-  function SponzorListController( $localStorage, sponzorshipService , utilsService) {
+  function SponzorListController( $localStorage, sponzorshipService , utilsService, $ionicPopover, $scope) {
 
     var vm = this;
+    var eventsPopover = null;
+    //Atributes
     vm.userAuth = $localStorage.userAuth;
     vm.sponsors = [];
+    vm.events = [];
     vm.showEmptyState = false;
+    vm.search = {};
+    vm.results = [];
+    //Accions
+    vm.filterByEvent = filterByEvent;
+    vm.showFilter = showFilter;
+
 
     activate();
 
     ////////////
 
     function activate(){
+      $ionicPopover.fromTemplateUrl('app/sponsors-organizer/events-popover.html', {
+        scope: $scope
+      }).then(function(popover) {
+        eventsPopover = popover;
+      });
       getSponsors();
     }
 
@@ -42,7 +58,7 @@
           utilsService.hideLoad();
           vm.showEmptyState = true;
           vm.sponsors = sponsors;
-          console.log( sponsors );
+          vm.events = getEvents( vm.sponsors );
         }
 
         function failed( error ){
@@ -50,6 +66,48 @@
           vm.showEmptyState = true;
           console.log( error );
         }
+    }
+
+    function getEvents( sponsors ){
+      return sponsors
+        .filter( filterByEvent )
+        .map( mapEvent );
+
+      function filterByEvent( item, pos, array ){
+        var position;
+        for (var i = 0; i < array.length; i++) {
+          if(array[i].event_id == item.event_id){
+            position = i;
+            break;
+          }
+        }
+        return position == pos;
+      }
+
+      function mapEvent( item ){
+        return {
+          title: item.title,
+          id: item.event_id
+        }
+      }
+    }
+
+    function showFilter($event) {
+      eventsPopover.show($event);
+    };
+
+    function closePopover() {
+      eventsPopover.hide();
+    };
+
+    function filterByEvent( idEvent ){
+      if(idEvent){
+        vm.search.event_id = idEvent;
+      }else{
+        vm.search = {};
+      }
+      
+      closePopover();
     }
     
 
