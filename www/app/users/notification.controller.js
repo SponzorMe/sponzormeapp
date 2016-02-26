1b6aea3744d656c178e0615866bd24eb0599b5e7
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-02-26 07:14:59 -0500

    Add firebase and angularfire

---
+++ www/app/users/notification.controller.js
@@ -0,0 +1,49 @@
+/**
+* @Controller for Forgot Password
+*
+* @author Carlos Rojas, Nicolas Molina
+* @version 0.2
+*/
+(function() {
+  'use strict';
+
+  angular
+    .module('app.users')
+    .controller('NotificationController', NotificationController);
+
+  ForgotController.$inject = [
+    '$translate',
+    'userService', 
+    '$state',
+    'utilsService',
+    '$ionicHistory'
+  ];
+
+  function NotificationController( $translate, userService, $state , utilsService, $ionicHistory) {
+
+    var vm = this;
+    vm.user = {};
+    vm.resetPassword = resetPassword;
+
+    ////////////
+
+    function resetPassword(){
+      utilsService.showLoad();
+      userService.forgotPassword( vm.user.email )
+        .then( complete )
+        .catch( failed );
+
+        function complete(){
+          utilsService.hideLoad();
+          $ionicHistory.clearCache();
+          $state.go("signin");
+          vm.user = {};
+        }
+
+        function failed( data ){
+          utilsService.hideLoad();
+        }
+    };
+
+  }
+})();
