fa8751c6410e0d6abb4d80c92b24d1d0a20c6930
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-03-19 02:11:53 -0400

    Scripts release

--- runme4Release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-ionic build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sponzorme.keystore platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk sponzorme
-~/Library/Android/sdk/build-tools/19.1.0/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk SponzorMeRelease.apk
--- runmeFirst.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-sudo npm install
-bower install
-ionic state restore
-ionic resources
