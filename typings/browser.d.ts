919484fed68abfdf1e9877c248c7310605946b02
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-03-29 12:00:05 -0400

    Organize typings

--- typings/browser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/cordova/index.d.ts" />
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/firebase/index.d.ts" />
-/// <reference path="browser/ambient/ionic/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
