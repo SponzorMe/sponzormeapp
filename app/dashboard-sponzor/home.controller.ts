d023b21a457e8b1718f72dc77a62c4858e8f8e71
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-03-30 16:13:14 -0400

    Overwrite HomeSponzorCtrl with tsc

--- app/dashboard-sponzor/home.controller.ts
+++ app/dashboard-sponzor/home.controller.ts
@@ -1,18 +1,17 @@
 /// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../services/user.service.ts" />
+/// <reference path="../services/userAuth.service.ts" />
+/// <reference path="../services/event.service.ts" />
+/// <reference path="../services/utils.service.ts" />
 /**
 * @Controller for Home Organizer
 *
 * @author Carlos Rojas, Nicolas Molina
 * @version 0.2
 */
-(function() {
-  'use strict';
-
-  angular
-    .module('app.dashboard-sponzor')
-    .controller('HomeSponzorController', HomeSponzorController);
-
-  HomeSponzorController.$inject = [
+class HomeSponzorCtrl{
+  
+  $inject = [
     '$localStorage',
     'userService',
     'utilsService',
@@ -20,52 +19,43 @@
     '$rootScope',
     'userAuthService'
   ];
-
-  function HomeSponzorController(  $localStorage, userService, utilsService, $scope, $rootScope, userAuthService) {
-
-    var vm = this;
-    //Attributes
-    vm.userAuth = userAuthService.getUserAuth();
-    vm.events = [];
-    //Funcions
-    vm.doRefresh = doRefresh;
-    
-    activate();
-    ////////////
-
-    function activate(){
-      vm.events = vm.userAuth.events.filter( filterDate );
-      $rootScope.$on('HomeSponzorController:getEvents', getEvents);
-    }
-    
-    function getEvents(event) {
-      vm.userAuth = userAuthService.getUserAuth();
-      vm.events = vm.userAuth.events.filter( filterDate );
-    }
-
-    function doRefresh(){
-      userService.home( vm.userAuth.id  )
-        .then( complete );
-        //.catch(failed );
-
-        function complete( user ){
-          vm.userAuth = userAuthService.updateUserAuth( user );
-          vm.events = vm.userAuth.events.filter( filterDate );
-          $scope.$broadcast('scroll.refreshComplete');
-        }
-
-        /*
-        function failed( error ){
-          console.log( error );
-          */
-        
-    }
-
-    function filterDate( item ){
-      return moment(item.starts).isAfter( new Date() );
-    }
-    
-    
-
+  userAuth:userModule.User;
+  events:eventModule.Event[] = [];
+  
+  constructor(
+    private $localStorage,
+    private userService: userModule.IUserService,
+    private utilsService: utilsServiceModule.IUtilsService,
+    private $scope: angular.IRootScopeService,
+    private $rootScope: angular.IRootScopeService,
+    private userAuthService: userAuthModule.IUserAuthService
+  ){
+    this.userAuth = userAuthService.getUserAuth();
+    this.events = this.userAuth.events.filter( this.filterDate );
   }
-})();
+  
+  registerListenerEvents() {
+    this.$rootScope.$on('HomeSponzorController:getEvents', () => {
+      this.userAuth = this.userAuthService.getUserAuth();
+      this.events = this.userAuth.events.filter( this.filterDate );
+    });
+  }
+  
+  doRefresh(){
+    this.userService.home( this.userAuth.id  )
+      .then( user => {
+        this.userAuth = this.userAuthService.updateUserAuth( user );
+        this.events = this.userAuth.events.filter( this.filterDate );
+        this.$scope.$broadcast('scroll.refreshComplete');
+      })
+      .catch(() => this.$scope.$broadcast('scroll.refreshComplete') );
+    }
+  
+  filterDate( item ){
+    return moment(item.starts).isAfter( new Date() );
+  }
+  
+}
+angular
+  .module('app.dashboard-sponzor')
+  .controller('HomeSponzorCtrl', HomeSponzorCtrl);
