3c9142215407f269763201dfa5543f321b7b4fc5
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-04-04 12:37:20 -0400

    Run test with coverage #179

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function( config ){
       //Tests
       //'tests/unit/**/*.service.spec.js',
       //'tests/unit/**/*.controller.spec.js',
-      'tests/unit/dashboard-organizer/intro.controller.spec.js'
+      'tests/unit/dashboard-sponzor/home.controller.spec.js'
     ]
   });
 };
