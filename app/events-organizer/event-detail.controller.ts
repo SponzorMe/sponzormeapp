baf424d60c50ad1db6d10b9e04c0faf36496ea6a
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-04-26 17:06:26 -0400

    Don't show organizer in event detail

--- app/events-organizer/event-detail.controller.ts
+++ app/events-organizer/event-detail.controller.ts
@@ -67,6 +67,7 @@ class EventDetailOrganizerCtrl{
     this.userAuth = this.userAuthService.getUserAuth();
     
     this.event = _.findWhere(this.userAuth.events, {id: $stateParams.id});
+    console.log( this.event );
     this.event.perks.forEach( this._preparatePerks, this );
     
     this.$ionicSideMenuDelegate.canDragContent(false);
